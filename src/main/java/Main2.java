c9c39fd9195ed4258ca7db3f3c35f37f45d5a8db
alex21072003 <alexkarpinskiy21@gmail.com>
2024-09-19 17:29:19 +0300

    Information

---
+++ src/main/java/Main2.java
@@ -0,0 +1,9 @@
+public class Main2 {
+    public static void main(String[] args) {
+        int amount = 420;
+        int maxAmount = 400;
+        if(amount > maxAmount){
+            System.out.println("Указанное колличество топлива превышает максимально допостимое");
+        }
+    }
+}
